76f3d0e486e114d37018115de569bf3dd691f3e7
unknown <bdrozdek@gmail.com>
2023-03-21 18:22:06 +0100

    created add task function

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let todoInput //miejsce gdzie użytkownik wpisuje treść zadania
 let errorInfo // info o braku zadań
 let addBtn // przycisk ADD dodaje nowe elementy do listy
 let ulLIst //list zadań, tagi UL
+let newTodo // nowo dodany LI, nowe zadanie
+
 
 const main = () => {
     prepareDOMElements()
@@ -15,7 +17,36 @@ const  prepareDOMElements = () => {
     ulLIst = document.querySelector('.todolist ul')
 }
 const prepareDOMEvents = () => {
-    
+    addBtn.addEventListener('click', addNewTodo)
+}
+
+const addNewTodo = () =>{
+    if (todoInput.value !=``){
+        newTodo = document.createElement('li')
+        newTodo.textContent = todoInput.value
+        createToolsArea()
+        ulLIst.append(newTodo)
+        todoInput.value =''
+        errorInfo.textContent = ''
+    } else{
+        errorInfo.textContent = 'Wpisz treść zadania'
+    }
+}
+
+const createToolsArea = () => {
+     const toolsPanel = document.createElement('div')
+     toolsPanel.classList.add('tools')
+     newTodo.append(toolsPanel)
+     const completeBtn = document.createElement('button')
+     completeBtn.classList.add('complete')
+     completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+     const editBtn = document.createElement('button')
+     editBtn.classList.add('edit')
+     editBtn.textContent= 'EDIT'
+     const deleteBtn = document.createElement('button')
+     deleteBtn.classList.add('delete')
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+     toolsPanel.append(completeBtn, editBtn, deleteBtn)
 }
 
 document.addEventListener('DOMContentLoaded', main)
